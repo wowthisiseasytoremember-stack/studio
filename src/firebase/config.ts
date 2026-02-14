fa50c1b66d4af166472ffd5b487251e98f85726e
Justin Ryan <wowthisiseasytoremember@gmail.com>
2026-02-14 04:20:17 +0000

    File changes

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,2 +1,9 @@
 // This file will be populated with your Firebase project's configuration.
-export const firebaseConfig = {};
+export const firebaseConfig = {
+  projectId: 'studio-8634274349-d356b',
+  appId: '1:885298539562:web:af34fa02bf69b04ef9e753',
+  apiKey: 'AIzaSyCjiKM1wF_5xvsWfAsVrPxZwweSYeo5Ngk',
+  authDomain: 'studio-8634274349-d356b.firebaseapp.com',
+  measurementId: '',
+  messagingSenderId: '885298539562',
+};
